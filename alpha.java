bc4619a3510c69b79c2a81bcbb4ec670e3761952
Asha2805 <43291441+Asha2805@users.noreply.github.com>
2018-09-26 07:36:50 -0400

    Update and rename README.md to  alpha.java

---
+++ alpha.java
@@ -0,0 +1,18 @@
+#import java.util.*;
+public class alpha
+{
+public static void main(String[] args)
+{
+char f;
+Scanner w=new Scanner(System.in);
+f=w.nextInt();
+if(f>='a'&&f<='z')||(f>='A'&&f<='Z')
+{
+System.out.println("Alphabet");
+}
+else
+{
+System.out.println("not Alphabet");
+}
+}
+}
